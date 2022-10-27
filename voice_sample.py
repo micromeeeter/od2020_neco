88694e66df297ab01d3ae967c089bb1decd146fa
Ryo Simon <46313038+ryo-simon-mf@users.noreply.github.com>
2022-10-27 14:27:34 +0900

    Add files via upload

---
+++ voice_sample.py
@@ -0,0 +1,11 @@
+import os
+from hatsuon import voiceSpeech as vs 
+
+def main():
+    print('start')
+    yomiage = vs.VoiceSpeech()
+    # vs = VoiceSpeech.VoiceSpeech()
+    yomiage.yoshi()
+
+if __name__ == '__main__':
+    main()
